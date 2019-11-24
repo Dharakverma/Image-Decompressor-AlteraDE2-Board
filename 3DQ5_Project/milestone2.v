9b3998cb1a12cf6ac0eac847dd48e7587cdcf0b3
Sakib Reza <sakibrezapc@gmail.com>
2019-11-24 00:41:07 -0500

    incomplete first stage

--- 3DQ5_Project/milestone2.v
+++ 3DQ5_Project/milestone2.v
@@ -67,31 +67,122 @@ logic write_en1;
 logic [31:0] data_out1;
 
 logic [7:0] S; // output is 8 bit unsigned
-logic [5:0] block_col; //to 40
-logic [4:0] block_row; //to 30
+logic [5:0] block_col; //to 39
+logic [4:0] block_row; //to 29
 logic [8:0] pixel_col;
 logic [7:0] pixel_row;
+logic [15:0] Sprime_row [2:0];
 
 always @(posedge CLOCK_50_I or negedge Resetn) begin
 	if (~Resetn) begin
+		pixel_col <= 6'b0;
+		//add the rest
 
 	end else begin
 
 		case(m2_state)
 			
 		idle: begin
-			
+			if (m2_start) begin
+				m2_state <= fill_lead_in0;
+
+			end
+
 		end
 
 		fill_lead_in0: begin
-			pixel_col <= block_col * 5'd8;
-			pixel_row <= block_row * 4'd8;
+		
+			write_en_n <= 1'b1;
+			SRAM_address <= ;		
 
-			SRAM_address <= pre_IDCT_offset + 
+			m2_state <= fill_lead_in1;
+		end
 
+		fill_lead_in1: begin
+			SRAM_address <= SRAM_address + 18'd1;
 
+			m2_state <= fill_lead_in2;
 		end
 		
+		fill_lead_in2: begin
+			SRAM_address <= SRAM_address + 18'd1;
+	
+			m2_state <= fill_CC0;
+		end
+
+		fill_CC0: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[0] <= SRAM_read_data;
+	
+			m2_state <= fill_CC1;
+		end
+
+		fill_CC1: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[1] <= SRAM_read_data;
+	
+			m2_state <= fill_CC2;
+		end
+
+		fill_CC2: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[2] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC3: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[3] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC4: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[4] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC5: begin
+			pixel_row <= pixel_row + 8'd1;
+
+			Sprime_row[5] <= SRAM_read_data;
+
+			address0 <= pixel_row 
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC6: begin
+			SRAM_address <= pre_IDCT_offset + pixel_col + pixel_row;
+
+			Sprime_row[6] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC7: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[7] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end
+
+		fill_CC8: begin
+			SRAM_address <= SRAM_address + 18'd1;
+
+			Sprime_row[2] <= SRAM_read_data;
+	
+			m2_state <= fill_CC3;
+		end 
 
 
 	end
